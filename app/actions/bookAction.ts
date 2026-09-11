87f77c2172b3462f623893d629c373dcf63f011f
Anukul Mahato <anukulmahato019@gmail.com>
2026-09-11 06:02:37 +0000

    Change Path API

--- app/actions/bookAction.ts
+++ app/actions/bookAction.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 
-const API = process.env.API_URL!;
+const API = "https://book-store-1-mhhy.onrender.com!";
 
 //Create Book
 export async function createBook(formData: FormData) {
